7fb027c7b811b3800cddbc3782c1cc4dda05a2b8
g3st1 <amogusik228123@hotmail.com>
2025-02-04 19:55:42 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -202,42 +202,30 @@ end
 
 local LeftGroupbox_Credits_1 = Credits:AddLeftGroupbox("Credits")
 do
-	LeftGroupbox_Credits_1:AddButton("first", {
+	LeftGroupbox_Credits_1:AddToggle("first", {
 		Text = "Inori - Main developer",
 		Default = false,
-		Func = function()
-			print("hi!!!")
-		end,
 	})
 end
 	
 do
-	LeftGroupbox_Credits_1:AddButton("second", {
+	LeftGroupbox_Credits_1:AddToggle("second", {
 		Text = "matas3535 - Creator of Splix.",
 		Default = false,
-		Func = function()
-			print("hi!!!")
-		end,
 	})
 end
 
 do
-	LeftGroupbox_Credits_1:AddButton("third", {
+	LeftGroupbox_Credits_1:AddToggle("third", {
 		Text = "Stefanuk - Extending library functionality.",
 		Default = false,
-		Func = function()
-			print("hi!!!")
-		end,
 	})
 end
 	
 do
-	LeftGroupbox_Credits_1:AddButton("fourth", {
+	LeftGroupbox_Credits_1:AddToggle("fourth", {
 		Text = "Wally - Cleaning up verbose code, extending library functionality.",
 		Default = false,
-		Func = function()
-			print("hi!!!")
-		end,
 	})
 end
 
